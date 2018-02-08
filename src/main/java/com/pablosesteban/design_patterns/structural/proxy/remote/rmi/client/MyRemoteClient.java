91a639b384592f2a4600ee4da4a472e6c314dbb4
Pablo Santamarta Esteban <pablosesteban@gmail.com>
2018-02-08 16:24:59 +0100

    structural - proxy

--- src/main/java/com/pablosesteban/design_patterns/structural/proxy/remote/rmi/client/MyRemoteClient.java
+++ src/main/java/com/pablosesteban/design_patterns/structural/proxy/remote/rmi/client/MyRemoteClient.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.pablosesteban.design_patterns.structural.proxy.rmi.client;
+package com.pablosesteban.design_patterns.structural.proxy.remote.rmi.client;
 
-import com.pablosesteban.design_patterns.structural.proxy.rmi.server.MyRemoteServer;
+import com.pablosesteban.design_patterns.structural.proxy.remote.rmi.server.MyRemoteServer;
 import java.io.IOException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
---
+++ src/main/java/com/pablosesteban/design_patterns/structural/proxy/virtual/ImageProxy.java
@@ -0,0 +1,16 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+package com.pablosesteban.design_patterns.structural.proxy.virtual;
+
+/**
+ *
+ * @author Pablo Santamarta Esteban <pablosesteban@gmail.com>
+ */
+public class ImageProxy {
+    public static void main(String[] args) {
+        
+    }
+}
