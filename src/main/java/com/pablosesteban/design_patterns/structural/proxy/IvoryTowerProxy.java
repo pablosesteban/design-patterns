1791734993b522ee98594dd2bd36d02e42b7b3c6
Pablo Santamarta Esteban <pablosesteban@gmail.com>
2018-02-16 12:21:39 +0100

    structural - proxy

--- src/main/java/com/pablosesteban/design_patterns/structural/proxy/IvoryTowerProxy.java
+++ src/main/java/com/pablosesteban/design_patterns/structural/proxy/IvoryTowerProxy.java
@@ -1,8 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package com.pablosesteban.design_patterns.structural.proxy;
 
 import java.util.logging.Logger;
@@ -13,10 +13,24 @@ import java.util.logging.Logger;
  */
 
 /*
-PROXY CLASS
-    keeps a reference to the Subject, so it can forward requests to the Subject when necessary
+DESIGN PATTERN: PROXY
+provides a surrogate or placeholder for another object to control access to it
 
-    often instantiates or handles the creation of the real subject (responsible for creating and destroying it)
+there are many variations of the pattern
+Remote Proxy: controls access to a remote object (object that lives in a different JVM)
+
+Virtual Proxy: controls access to a resource that is expensive to create
+
+Protection Proxy: controls access to a resource based on access rights
+
+use the Proxy Pattern to create a representative object that controls access to another object
+*/
+
+/*
+PROXY CLASS
+keeps a reference to the Subject, so it can forward requests to the Subject when necessary
+
+often instantiates or handles the creation of the real subject (responsible for creating and destroying it)
 */
 public class IvoryTowerProxy implements WizardTower {
     private static final Logger LOGGER = Logger.getLogger(IvoryTowerProxy.class.getName());
@@ -27,7 +41,7 @@ public class IvoryTowerProxy implements WizardTower {
     
     // real subject
     private IvoryTower ivoryTower;
-
+    
     public IvoryTowerProxy(IvoryTower ivoryTower) {
         this.ivoryTower = ivoryTower;
     }
@@ -42,5 +56,14 @@ public class IvoryTowerProxy implements WizardTower {
         }else {
             LOGGER.severe(w + " cannot enter, tower if full of wizards!");
         }
+    }
+    
+    public static void main(String[] args) {
+        IvoryTowerProxy proxy = new IvoryTowerProxy(new IvoryTower());
+        
+        proxy.enter(new Wizard("Black Wizard"));
+        proxy.enter(new Wizard("Red Wizard"));
+        proxy.enter(new Wizard("White Wizard"));
+        proxy.enter(new Wizard("Yellow Wizard"));
     }
 }
